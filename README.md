0142263379c54a7bbae3ffb001191e023e5e9343
g-barrett <barrettg516@gmail.com>
2024-01-01 20:35:28 -0600

    added questions and set up file structure

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # READEME.md-Generator
 A simple node.js application ran through your terminal that takes your inputs and creates a README.md file you can use in your repository
+
+
+credits
+www.npmjs.com/package/inquirer
+https://www.geeksforgeeks.org/node-js-fs-writefilesync-method/#
+https://www.w3schools.com/nodejs/met_path_join.asp
---
+++ index.js
@@ -0,0 +1,59 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const generateMd = require("./utils/generateMarkdown");
+
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description of this project?',
+    },
+    {
+        type: 'input',
+        name: 'motivation',
+        message: 'What was the motivation behind the project?',
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'What are the steps for installation for your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use your application?',
+    },
+    {
+        type: 'input',
+        name: 'credit',
+        message: 'List out any sources that should be credited.',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license will you be using?',
+        choices: ['None', 'MIT', 'Apache 2.0', 'ISC', 'BSD 3-Clause', 'BSD 2-Clause'],
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How can your project be tested?',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your Github username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Finally, what is your Email address?',
+    }
+];
+
